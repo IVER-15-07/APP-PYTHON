bd17fa6c7f0ec6a796ac142a3e058e196fb5e4fd
iver <limber123iver654.12@gmail.com>
2025-09-22 15:39:41 -0400

    prueba de  primer commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div>
+      <h1>hola  como estas 
+         
+      </h1>
+         {/* Botón Tailwind */}
+      <button
+        type="button"
+        className="mt-4 inline-flex items-center rounded-lg bg-blue-600 px-4 py-2 text-white shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 active:bg-blue-800"
+        onClick={() => console.log('Botón clic')}
+      >
+        Botón Tailwind
+      </button>
+     </div>
     </>
   )
 }

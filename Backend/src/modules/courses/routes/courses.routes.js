cb33abba1aed811f45c872bf716cc1601fb46dea
iver <limber123iver654.12@gmail.com>
2025-11-08 16:52:31 -0400

    fix: route couses

--- Backend/src/modules/courses/routes/courses.routes.js
+++ Backend/src/modules/courses/routes/courses.routes.js
@@ -10,7 +10,7 @@ import {
 
 } from '../controllers/courses.Controller.js';
 import multer from 'multer';
-//import { verifyToken } from '../../auth/middleware/auth.middleware.js';
+import { verifyToken } from '../../auth/middleware/auth.middleware.js';
 
 
 
@@ -18,12 +18,12 @@ const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.get('/my-courses', getMyCourses);
-router.post('/topic',  upload.array('files'), createTopicWithResource);
+router.post('/topic', verifyToken, upload.array('files'), createTopicWithResource);
 router.get('/topics', getAllTopics);
 router.get('/topic-types', getTopicTypes);
 router.get('/levels', getLevels);
-router.put('/course/:id',updateCourse);
-router.put('/topic/:id', upload.array('files'), updateTopic);
+router.put('/course/:id', verifyToken, updateCourse);
+router.put('/topic/:id', verifyToken, upload.array('files'), updateTopic);
 
 
 

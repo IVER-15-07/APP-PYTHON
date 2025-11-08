345a6eda2cbd5be3d8760357b403b8c92a8c6c10
iver <limber123iver654.12@gmail.com>
2025-11-08 18:01:51 -0400

    feat : add topicService route

--- Frontend/services/topic.api.js
+++ Frontend/services/topic.api.js
@@ -48,4 +48,22 @@ export const topicsService = {
             throw new Error(error.response?.data?.message || 'Error al obtener los niveles');
         }
     },
-}
+
+    async updateTopic(topicId, topicData, files) {
+        try {
+            const formData = new FormData();    
+            Object.entries(topicData).forEach(([key, value]) => {
+                formData.append(key, value);
+            });
+            files.forEach((file) => {
+                formData.append("files", file);
+            });
+            const response = await axiosInstance.put(`/api/topic/${topicId}`, formData, {
+                headers: { "Content-Type": "multipart/form-data" },
+            });
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response?.data?.message || "Error al actualizar el tópico");
+        }
+    }
+};

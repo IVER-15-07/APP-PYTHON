0de78205ece0e3b3b72649e5a81c72a90e061e52
iver <limber123iver654.12@gmail.com>
2025-12-01 16:25:06 -0400

    segundo deploy

--- Frontend/helpers/axios-config.js
+++ Frontend/helpers/axios-config.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-
+const BASE_URL = import.meta.env.VITE_API_URL ;
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',

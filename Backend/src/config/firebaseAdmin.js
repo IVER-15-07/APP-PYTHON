65da4d3f4bf0a304db1297eb108f315bacca3204
iver <limber123iver654.12@gmail.com>
2025-12-03 14:22:04 -0400

    se elimino la  ruta local

--- Backend/src/config/firebaseAdmin.js
+++ Backend/src/config/firebaseAdmin.js
@@ -1,38 +1,22 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
 
-// 1. Intentar leer la variable de entorno (PRODUCCIÓN)
+
 const rawServiceAccount = process.env.FIREBASE_CREDENTIALS;
 
-if (rawServiceAccount) {
-  try {
-    const serviceAccount = JSON.parse(rawServiceAccount);
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-    console.log("✅ Firebase Admin inicializado desde variable de entorno.");
-  } catch (e) {
-    console.error("❌ ERROR: No se pudo parsear el JSON de Firebase desde ENV.", e);
-    process.exit(1); // Detener la app si no se puede inicializar
-  }
-} else {
- 
-  try {
-    const localPath = path.resolve("src/config/login-ac512-firebase-adminsdk-fbsvc-14457b84d6.json");
-    if (!fs.existsSync(localPath)) {
-      throw new Error(`Archivo no encontrado en ${localPath}`);
-    }
+if (!rawServiceAccount) {
+  console.error(" ERROR: No se encontró la variable de entorno FIREBASE_CREDENTIALS");
+  process.exit(1);
+}
 
-    const serviceAccount = JSON.parse(fs.readFileSync(localPath, "utf8"));
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-    console.log("✅ Firebase Admin inicializado desde archivo local.");
-  } catch (e) {
-    console.error("❌ ERROR: No se pudo inicializar Firebase Admin localmente.", e);
-    process.exit(1); // Detener la app si falla
-  }
+try {
+  const serviceAccount = JSON.parse(rawServiceAccount);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  console.log(" Firebase Admin inicializado correctamente desde variable de entorno.");
+} catch (e) {
+  console.error("ERROR: No se pudo parsear el JSON de Firebase desde FIREBASE_CREDENTIALS", e);
+  process.exit(1);
 }
 
 export default admin;

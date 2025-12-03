a199aa0b554a99b2e46e4ed19f222ad022188174
iver <limber123iver654.12@gmail.com>
2025-12-03 12:58:31 -0400

    variable de entorno

--- Frontend/helpers/axios-config.js
+++ Frontend/helpers/axios-config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: "https://localhost:3000",
+  baseURL: "https://backend-production-45c2.up.railway.app",
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
